b3bd9d9d2429578d660ad59712f424029f904df8
Mkiterian <hmukite@gmail.com>
2017-06-14 13:48:29 +0300

    Initial Commit

---
+++ exchange_rates.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+key = '602a08ec0a5f86942c1cc00b985352d9'
+
+def get_current_usd_rate_vs_world_currencies():
+	#Uses the currencylayer.com API
+	baseurl = 'http://www.apilayer.net/api/live'
+
+	my_attrs = {'access_key': key}
+
+	response =  requests.get(baseurl, params=my_attrs)
+
+	print(response.status_code)
+
+	js_data = response.json()
+
+	return js_data['quotes']
+
+rates = get_current_usd_rate_vs_world_currencies()
+
+for rate in rates:
+	print('USD to {} : {} '.format(rate[3:], rates[rate]))
