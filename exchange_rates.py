a5e39260a3a4b847735793e2b5a1707e7c64450a
Mkiterian <hmukite@gmail.com>
2017-06-14 16:46:00 +0300

    Change function name

--- exchange_rates.py
+++ exchange_rates.py
@@ -3,19 +3,20 @@ import requests
 
 key = '602a08ec0a5f86942c1cc00b985352d9'
 
-def get_current_usd_rate_vs_world_currencies():
-	#Uses the currencylayer.com API
-	baseurl = 'http://www.apilayer.net/api/live'
 
-	my_attrs = {'access_key': key}
+def get_current_usd_vs_world_currencies():
+    # Uses the currencylayer.com API
+    baseurl = 'http://www.apilayer.net/api/live'
 
-	response =  requests.get(baseurl, params=my_attrs)
+    my_attrs = {'access_key': key}
 
-	js_data = response.json()
+    response = requests.get(baseurl, params=my_attrs)
 
-	return js_data['quotes']
+    js_data = response.json()
 
-rates = get_current_usd_rate_vs_world_currencies()
+    return js_data['quotes']
+
+rates = get_current_usd_vs_world_currencies()
 
 for rate in rates:
-	print('USD to {} : {} '.format(rate[3:], rates[rate]))
+    print('USD to {} : {} '.format(rate[3:], rates[rate]))
